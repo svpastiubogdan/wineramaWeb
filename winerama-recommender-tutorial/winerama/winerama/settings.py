6d39b83117785784ff1434a220825f73040c61d4
bpastiu <bogdan.pastiu@softvision.ro>
2019-03-20 15:17:02 +0200

    checked out new version of app

--- winerama-recommender-tutorial/winerama/winerama/settings.py
+++ winerama-recommender-tutorial/winerama/winerama/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap3',
     'reviews',
 )
 
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'winerama.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
